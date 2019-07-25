96dc31406291cc4d18a26dd624d8524b1f694db4
Giovanni-P-Meloni <gio.mel@hotmail.com>
2019-07-25 14:56:27 -0300

    MenuInicial prototipo

---
+++ Assets/MainMenu.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1); //FIXME: Need to make sure that this is right
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quit");
+        Application.Quit();
+    }
+}
+    
